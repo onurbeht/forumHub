a0e0b41781136850e722d3f6b074c701be948fd0
onurbeht <brunnobatista25@gmail.com>
2024-10-29 17:14:05 -0300

    Project started

---
+++ src/test/java/com/forumHub/ForumhubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.forumHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForumhubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
